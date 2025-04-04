e7fe896b7e0b625c031eeac1ae5ce416677927ec
kallbuloso <kallbuloso@gmail.com>
2025-04-04 20:10:02 -0300

    chore: Add Permission model with searchable trait and date attribute accessors

---
+++ app/Models/Permission.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\SearchableTrait;
+use Spatie\Permission\Models\Permission as OriginalPermission;
+
+/**
+ * Class Permission
+ *
+ * @property $id
+ * @property $name
+ * @property $description
+ * @property $guard_name
+ * @property $created_at
+ * @property $updated_at
+ *
+ * @package App
+ * @mixin \Illuminate\Database\Eloquent\Builder
+ */
+class Permission extends OriginalPermission
+{
+    use SearchableTrait;
+
+    protected $table = "permissions";
+
+    /**
+     * Attributes that should be mass-assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'description',
+        'guard_name'
+    ];
+
+    protected $searchable = [
+        'name'
+    ];
+
+    public function getCreatedAtAttribute($value)
+    {
+        return date("d/m/Y H:i:s", strtotime($value));
+    }
+
+    public function getUpdatedAtAttribute($value)
+    {
+        return date('d/m/Y H:i:s', strtotime($value));
+    }
+
+    public function getDeletedAtAttribute($value)
+    {
+        return date('d/m/Y H:i:s', strtotime($value));
+    }
+}
