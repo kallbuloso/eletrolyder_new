7239778c6f13e4ed697cd20b4a2a9524e9697269
kallbuloso <kallbuloso@gmail.com>
2025-04-04 19:34:07 -0300

    chore: Add renderPage method to Controller class

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use Inertia\Inertia;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
 abstract class Controller
 {
-    //
+    use AuthorizesRequests;
+
+    /**
+     * Render a page component.
+     *
+     * @param string $component
+     * @param array|null $props
+     * @return \Inertia\Response
+     */
+    public function renderPage(string $component, array $props = null): \Inertia\Response
+    {
+        return Inertia::render($component, $props);
+    }
+
+    // /**
+    //  * Render a modal component.
+    //  *
+    //  * @param string $component
+    //  * @param array|null $props
+    //  * @param string|array $baseRoute
+    //  * @return \Momentum\Modal\Modal
+    //  */
+    // public function renderModal(string $component, array $props = null): \Momentum\Modal\Modal
+    // {
+    //   return Inertia::modal($component)
+    //     ->with($props);
+    // }
 }
