5710dcffc6272477facbdbb06ee786462ad2b9e6
kallbuloso <kallbuloso@gmail.com>
2025-04-05 20:33:46 -0300

    refactor: Add PhoneableTrait for handling phone-related operations

---
+++ app/Traits/PhoneableTrait.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Traits;
+
+use App\Models\Phone;
+
+trait PhoneableTrait
+{
+
+    public function phones()
+    {
+        return $this->morphMany(Phone::class, 'phoneable');
+    }
+
+    public static function bootPhoneableTrait()
+    {
+        static::deleting(function ($model) {
+            $model->phones()->delete();
+        });
+    }
+}
