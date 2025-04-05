10ed7d5faa5bbd71e7e78735b1f204aa4fd1e647
kallbuloso <kallbuloso@gmail.com>
2025-04-05 19:07:05 -0300

    refactor: Add Address model with traits and relationships

---
+++ app/Models/Address.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\SearchableTrait;
+use App\Traits\AddressableTrait;
+use App\Traits\DatesModelTraits;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+/**
+ * Class Address
+ *
+ * @property $id
+ * @property $type
+ * @property $street
+ * @property $number
+ * @property $complement
+ * @property $neighborhood
+ * @property $city
+ * @property $state
+ * @property $country
+ * @property $zip_code
+ * @property $reference
+ *
+ * @package App
+ * @mixin \Illuminate\Database\Eloquent\Builder
+ */
+class Address extends Model
+{
+    use HasFactory, SearchableTrait, DatesModelTraits, AddressableTrait;
+
+    protected $table = "addresses";
+
+    protected $perPage = 10;
+
+    protected $guarded = ['id'];
+
+    /**
+     * Attributes that should be mass-assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'type',
+        'street',
+        'number',
+        'complement',
+        'neighborhood',
+        'city',
+        'state',
+        'country',
+        'zip_code',
+        'reference'
+    ];
+
+    protected $searchable = [
+        'type',
+        'street',
+        'number',
+        'complement',
+        'neighborhood',
+        'city',
+        'state',
+        'country',
+        'zip_code',
+        'reference'
+    ];
+
+    public function addressable()
+    {
+        return $this->morphTo();
+    }
+}
