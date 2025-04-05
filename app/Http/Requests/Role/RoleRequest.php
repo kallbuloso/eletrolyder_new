cc717b4bad6e7f5ed52adc44d67c2d6dcb26423c
kallbuloso <kallbuloso@gmail.com>
2025-04-05 16:56:56 -0300

    refactor: Update RoleRequest to include validation rules for role creation and update

---
+++ app/Http/Requests/Role/RoleRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Requests\Role;
+
+use Illuminate\Validation\Rule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class RoleRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name'          => ['required', 'string', 'max:100', Rule::unique('roles')->ignore($this->id)],
+            'description'    => 'nullable|string',
+            'guard_name' => 'required|string',
+            'permissions.*' => 'nullable|exists:permissions,id',
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'Nome do acesso é obrigatório.',
+            'name.unique' => 'Já existe um acesso com esse nome.',
+        ];
+    }
+}
