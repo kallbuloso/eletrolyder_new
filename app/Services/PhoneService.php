3946c6acc1d40182ba79164e1edddbd97512473d
kallbuloso <kallbuloso@gmail.com>
2025-04-05 20:33:40 -0300

    refactor: Create PhoneService for handling phone-related operations

---
+++ app/Services/PhoneService.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Phone;
+
+class PhoneService extends BaseService
+{
+  public function __construct(Phone $service)
+  {
+    $this->model = $service;
+  }
+}
