ff42641c8baa9dc63fe8f3c6404e601b913b1c29
kallbuloso <kallbuloso@gmail.com>
2025-04-05 20:33:21 -0300

    refactor: Add PhoneRequest for handling phone validation rules

---
+++ app/Http/Requests/PhoneRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PhoneRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'phone_type' => ['required', 'string'],
+            'phone_number' => ['required', 'string'],
+            'phone_contact' => ['nullable', 'required_if:phone_type,C', 'string'],
+            'phone_has_whatsapp' => ['required', 'boolean'],
+        ];
+    }
+}
