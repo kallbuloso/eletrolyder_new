e33099f795a99268e79c942565bae5f644734def
kallbuloso <kallbuloso@gmail.com>
2025-04-05 18:20:29 -0300

    feat: Create migration for phones table

---
+++ database/migrations/2024_12_25_234325_create_phones_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('phones', function (Blueprint $table) {
+            $table->id();
+            $table->enum('phone_type', ['P', 'C'])->default('P'); // P - Principal, C - Contato
+            $table->string('phone_number')->nullable();
+            $table->string('phone_contact')->nullable();
+            $table->boolean('phone_has_whatsapp')->default(false);
+            $table->morphs('phoneable'); // phoneable_id, phoneable_type
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('phones');
+    }
+};
