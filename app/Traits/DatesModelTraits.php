db9b99919c7ec2a5f3a82a85db62871d96ccb7fc
kallbuloso <kallbuloso@gmail.com>
2025-04-05 17:43:26 -0300

    refactor: Add DatesModelTraits to handle date formatting in models

---
+++ app/Traits/DatesModelTraits.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Traits;
+
+trait DatesModelTraits
+{
+  public function getEmailVerifiedAtAttribute($value)
+  {
+    return $this->checkDate($value);
+  }
+
+  public function getCreatedAtAttribute($value)
+  {
+    return $this->checkDate($value);;
+  }
+
+  public function getUpdatedAtAttribute($value)
+  {
+    return $this->checkDate($value);
+  }
+
+  public function getDeletedAtAttribute($value)
+  {
+    return $this->checkDate($value);
+  }
+  private function checkDate($value)
+  {
+    return $value ? date('d/m/Y H:i:s', strtotime($value)) : null;
+  }
+}
