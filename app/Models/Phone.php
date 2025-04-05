d6822a03011eb60e5eec84eecd4916f0879111e3
kallbuloso <kallbuloso@gmail.com>
2025-04-05 20:33:30 -0300

    refactor: Add Phone model with traits and relationships

---
+++ app/Models/Phone.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\PhoneableTrait;
+use App\Traits\SearchableTrait;
+use App\Traits\DatesModelTraits;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+/**
+ * Class Phone
+ *
+ * @property $id
+ * @property $phone_type
+ * @property $phone_number
+ * @property $phone_contact
+ * @property $phone_has_whatsapp
+ * @property $created_at
+ * @property $updated_at
+ *
+ * @package App
+ * @mixin \Illuminate\Database\Eloquent\Builder
+ */
+class Phone extends Model
+{
+    use HasFactory, SearchableTrait, DatesModelTraits, PhoneableTrait;
+
+    protected $table = "phones";
+
+    protected $perPage = 10;
+
+    protected $guarded = ['id'];
+
+    /**
+     * Attributes that should be mass-assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'phone_type',
+        'phone_number',
+        'phone_contact',
+        'phone_has_whatsapp',
+    ];
+
+    protected $searchable = [
+        'phone_type',
+        'phone_number',
+        'phone_contact',
+        'phone_has_whatsapp'
+    ];
+
+    public function phoneable()
+    {
+        return $this->morphTo();
+    }
+}
