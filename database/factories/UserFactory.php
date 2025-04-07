31f8674d0b5bf429de7075f95253f15cb0dc919c
kallbuloso <kallbuloso@gmail.com>
2025-04-06 21:10:49 -0300

    refactor: Add 'tenant_id' field to UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,6 +24,7 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
+            'tenant_id' => Str::random(10),
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
