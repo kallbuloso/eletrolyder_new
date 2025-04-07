e5dc1b3da9b30b3d3f5a0273a07ff32ef2027d3e
kallbuloso <kallbuloso@gmail.com>
2025-04-06 23:59:38 -0300

    refactor: Update 'tenant_id' validation in UserRequest to require and validate the 'id' field and 'tenant_id' field

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -26,10 +26,24 @@ class UserRequest extends FormRequest
         return [
             'tenant_id' => ['required', 'exists:tenants,id', 'integer'],
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', Rule::unique('users')->ignore($this->id)],
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:users,tenant_id'],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
             'profile_photo_path' => ['string', 'nullable'],
-            'roles.*' => ['nullable', 'exists:roles,id'],
+            'roles.*' => ['required', 'exists:roles,id'],
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'Nome do usuário é obrigatório.',
+            'email.unique' => 'Já existe um usuário com esse email.',
+            'password.required' => 'Senha do usuário é obrigatória.',
         ];
     }
 }
