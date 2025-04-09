b24c933daaeedfdebac3f82ed7f6aa4a6a8a5949
kallbuloso <kallbuloso@gmail.com>
2025-04-08 21:43:51 -0300

    refactor: Add ClientTrait for managing client data

---
+++ app/Traits/ClientTrait.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+trait ClientTrait
+{
+    use HasFactory, PhoneableTrait, AddressableTrait, TenantebleTrait, SearchableTrait, DatesModelTraits;
+}
