0dff4ae30c2e33c6678cf1a9fbee879f75b52830
kallbuloso <kallbuloso@gmail.com>
2025-04-10 20:32:28 -0300

    refactor: Create migration for suppliers table

---
+++ database/migrations/2025_01_05_153211_create_suppliers_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('suppliers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('tenant_id')->nullable()->constrained();
+            $table->string('name')->required();
+            $table->string('nick_name')->nullable();
+            $table->string('contact')->nullable();
+            // Documento
+            $table->enum('person', ['F', 'J'])->default('F'); // F - Física, J - Jurídica
+            $table->string('cpf_cnpj')->nullable();
+            $table->string('birth_date')->nullable();
+            // Avatar
+            $table->string('avatar')->nullable();
+            // site e E-mail
+            $table->string('site')->nullable();
+            $table->string('email')->nullable();
+            // Observações
+            $table->text('note')->nullable();
+            // Status
+            $table->enum('status', ['A', 'I', 'B'])->default('A'); // A - Ativo, I - Inativo, B - Bloqueado
+            $table->string('blocking_reason')->nullable(); // Motivo do bloqueio
+            $table->string('last_purchase')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('supliers');
+    }
+};
