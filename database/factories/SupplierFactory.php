a5b8232026219caccdb3db0c2152b5dc1ef55494
kallbuloso <kallbuloso@gmail.com>
2025-04-10 20:32:21 -0300

    refactor: Add SupplierFactory for generating supplier data

---
+++ database/factories/SupplierFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Supplier>
+ */
+class SupplierFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $person = fake()->randomElement(['F', 'J']);
+        $status = fake()->randomElement(['A', 'I', 'B']);
+        return [
+            'name' => fake()->name(),
+            'nick_name' => fake()->lastName(),
+            'contact' => fake()->firstName(),
+            'person' => $person,
+            'cpf_cnpj' => $person === 'F' ? fake()->unique()->cpf(false) : fake()->unique()->cnpj(false),
+            'birth_date' => fake()->date('d/m/Y'),
+            'site' => fake()->url(),
+            'email' => fake()->unique()->safeEmail(),
+            'note' => fake()->paragraph(4),
+            'status' => $status,
+            'blocking_reason' => $status === 'B' ? fake()->sentence() : null,
+            'last_purchase' => fake()->date('d/m/Y H:i:s'),
+        ];
+    }
+}
