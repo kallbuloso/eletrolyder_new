b1d112d1b96108a27873017056def5346a4136a3
kallbuloso <kallbuloso@gmail.com>
2025-04-10 20:32:09 -0300

    refactor: Add SupplierTrait for reusable supplier functionality

---
+++ app/Traits/SupplierTrait.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+trait SupplierTrait
+{
+    use HasFactory, PhoneableTrait, AddressableTrait, TenantebleTrait, SearchableTrait, DatesModelTraits;
+    //
+}
