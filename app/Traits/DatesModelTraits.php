ca78acb20f5ec7e8344d0374096e967d48d7f327
kallbuloso <kallbuloso@gmail.com>
2025-04-11 20:09:49 -0300

    refactor: Update DatesModelTraits to include getLastPurchaseAttribute method

--- app/Traits/DatesModelTraits.php
+++ app/Traits/DatesModelTraits.php
@@ -4,27 +4,33 @@ namespace App\Traits;
 
 trait DatesModelTraits
 {
-  public function getEmailVerifiedAtAttribute($value)
-  {
-    return $this->checkDate($value);
-  }
+    public function getEmailVerifiedAtAttribute($value)
+    {
+        return $this->checkDate($value);
+    }
 
-  public function getCreatedAtAttribute($value)
-  {
-    return $this->checkDate($value);;
-  }
+    public function getCreatedAtAttribute($value)
+    {
+        return $this->checkDate($value);;
+    }
 
-  public function getUpdatedAtAttribute($value)
-  {
-    return $this->checkDate($value);
-  }
+    public function getUpdatedAtAttribute($value)
+    {
+        return $this->checkDate($value);
+    }
 
-  public function getDeletedAtAttribute($value)
-  {
-    return $this->checkDate($value);
-  }
-  private function checkDate($value)
-  {
-    return $value ? date('d/m/Y H:i:s', strtotime($value)) : null;
-  }
+    public function getDeletedAtAttribute($value)
+    {
+        return $this->checkDate($value);
+    }
+
+    public function getLastPurchaseAttribute($value)
+    {
+        return $this->checkDate($value);
+    }
+
+    private function checkDate($value)
+    {
+        return $value ? date('d/m/Y', strtotime($value)) : null;
+    }
 }
