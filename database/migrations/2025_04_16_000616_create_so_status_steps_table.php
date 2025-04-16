d8e426091bec23831f47cb5c34fae28a3a271150
kallbuloso <kallbuloso@gmail.com>
2025-04-15 21:35:36 -0300

    refactor: Create migration for SO Status Steps table

---
+++ database/migrations/2025_04_16_000616_create_so_status_steps_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('so_status_steps', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('tenant_id')->nullable()->constrained();
+            $table->foreignId('status_id')->nullable()->constrained('so_statuses');
+            // Colunas de relacionamento polimórfico (por exemplo: stepable_id e stepable_type)
+            $table->morphs('stepable');
+            $table->string('name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('so_status_steps');
+    }
+};
