71f2ffbb912d35282b7068d3dcdd957c1fe256b2
kallbuloso <kallbuloso@gmail.com>
2025-04-26 17:03:08 -0300

    feat: Adicionar SoStatusService para gerenciar a lógica de negócios dos status de pedidos de serviço

---
+++ app/Services/SoStatusService.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\SoStatus;
+
+class SoStatusService extends BaseService
+{
+  public function __construct(SoStatus $service)
+  {
+    $this->model = $service;
+  }
+}
