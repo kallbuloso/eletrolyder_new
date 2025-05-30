9c7d0d53da6efac58eff53f5747509fd11ae408c
kallbuloso <kallbuloso@gmail.com>
2025-05-29 21:45:54 -0300

    feat: Corrigir valores de status e razão de bloqueio na fábrica de clientes

--- database/factories/ClientFactory.php
+++ database/factories/ClientFactory.php
@@ -17,7 +17,7 @@ class ClientFactory extends Factory
     public function definition(): array
     {
         $person = fake()->randomElement(['F', 'J']);
-        $status = fake()->randomElement(['A', 'I', 'B']);
+        $status = fake()->randomElement([0, 1, 2]); // 0 - Ativo, 1 - Inativo, 2 - Bloqueado
         return [
             'name' => fake()->name(),
             'nick_name' => fake()->lastName(),
@@ -26,7 +26,7 @@ class ClientFactory extends Factory
             'cpf_cnpj' => $person === 'F' ? fake()->unique()->cpf(false) : fake()->unique()->cnpj(false),
             'note' => fake()->paragraph(2),
             'status' => $status,
-            'blocking_reason' => $status === 'B' ? fake()->sentence() : null,
+            'blocking_reason' => $status === 2 ? fake()->sentence() : null,
             'last_purchase' => fake()->date('d/m/Y H:i:s'),
         ];
     }
