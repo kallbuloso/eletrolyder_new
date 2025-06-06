72497df496078c6f4be5282de71c7578f497e93c
kallbuloso <kallbuloso@gmail.com>
2025-06-05 22:43:17 -0300

    feat: adicionar componente Create.vue para gerenciamento de status de pedidos

---
+++ resources/js/Pages/ServicesOrder/Statuses/Create.vue
@@ -0,0 +1,80 @@
+<script setup>
+const props = defineProps({
+  title: {
+    type: [Object, Array],
+    required: true
+  },
+  errors: {
+    type: Object,
+    default: () => ({})
+  }
+})
+
+const form = useForm({
+  description: '',
+  status_type: '0',
+  generates_revenue: true
+})
+
+const submit = () => {
+  form.post(route('orders.soStatus.store'), {
+    onSuccess: () => form.reset()
+  })
+}
+
+const statusTypes = [
+  { title: 'Entada', value: '0' },
+  { title: 'Em Andamento', value: '1' },
+  { title: 'Saída', value: '2' }
+]
+</script>
+
+<template>
+  <app-modal width="600">
+    <v-form @submit.prevent="submit">
+        <v-card :prepend-icon="props.data ? 'mdi-pencil' : 'mdi-plus'" :title="props.title">
+            <v-card-text class="mx-1">
+                <v-row >
+                    <v-col cols="6">
+                        <app-text-field
+                            id="description"
+                            v-model="form.description"
+                            label="Descrição"
+                            placeholder="Descrição do status"
+                            required
+                            :error-messages="form.errors.description"
+                        />
+                    </v-col>
+                    <v-col cols="6">
+                        <app-select
+                            id="status_type"
+                            v-model="form.status_type"
+                            :items="statusTypes"
+                            label="Tipo de Status"
+                            placeholder="Selecione o tipo de status"
+                            required
+                            :error-messages="form.errors.status_type"
+                        />
+                    </v-col>
+                    <v-spacer />
+                    <v-col cols="6">
+                        <app-checkbox
+                            id="generates_revenue"
+                            v-model="form.generates_revenue"
+                            title="Gera Receita?"
+                            :error-messages="form.errors.generates_revenue"
+                        />
+                    </v-col>
+                </v-row>
+            </v-card-text>
+        <v-card-actions class="mx-4">
+          <v-spacer />
+          <Link :href="route('orders.soStatus.index')" as="div">
+            <v-btn class="mr-2" variant="text">Cancelar</v-btn>
+          </Link>
+          <v-btn type="submit" color="primary" variant="flat" :loading="form.processing">Salvar</v-btn>
+        </v-card-actions>
+        </v-card>
+    </v-form>
+    </app-modal>
+</template>
