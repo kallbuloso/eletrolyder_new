05f5f03be85f4a253943b5de16fad36266a331f1
kallbuloso <kallbuloso@gmail.com>
2025-06-06 21:20:03 -0300

    refactor: ajustar propriedades e layout do formulário de edição de status

--- resources/js/Pages/ServicesOrder/Statuses/Edit.vue
+++ resources/js/Pages/ServicesOrder/Statuses/Edit.vue
@@ -1,8 +1,8 @@
 <script setup>
 const props = defineProps({
   data: {
-    type: Array,
-    required: true
+    type: [Object, Array],
+    required: false
   },
   title: {
     type: [Object, Array],
@@ -14,15 +14,25 @@ const props = defineProps({
   }
 })
 
+const value = shallowRef(props.data.status_type || '0')
+
 const form = useForm({
+  id: props.data.id,
+  tenant_id: usePage().props.auth.user.tenant_id,
   description: props.data.description,
-  status_type: props.data.status_type,
+  status_type: value.value.toString(),
   generates_revenue: props.data.generates_revenue
 })
 
 const submit = () => {
-  form.post(route('orders.soStatus.update'), {
-    onSuccess: () => form.reset()
+  form.put(route('orders.soStatus.update', props.data.id), {
+    onSuccess() {
+      form.reset()
+      router.visit(route('orders.soStatus.index'), {
+        preserveScroll: true,
+        preserveState: false
+      })
+    }
   })
 }
 
@@ -34,37 +44,38 @@ const statusTypes = [
 </script>
 
 <template>
-  <app-modal width="600">
+  <app-modal width="700">
     <v-form @submit.prevent="submit">
       <v-card :prepend-icon="'mdi-plus'" :title="props.title">
         <v-card-text class="mx-1">
           <v-row>
-            <v-col cols="6">
+            <v-col cols="5">
               <app-text-field id="description" v-model="form.description" label="Descrição" placeholder="Descrição do status" required :error-messages="form.errors.description" />
             </v-col>
-            <v-col cols="6">
+            <v-col cols="4">
               <app-select
                 id="status_type"
                 v-model="form.status_type"
                 :items="statusTypes"
+                item-title="title"
+                item-value="value"
                 label="Tipo de Status"
                 placeholder="Selecione o tipo de status"
                 required
                 :error-messages="form.errors.status_type"
               />
             </v-col>
-            <v-spacer />
-            <v-col cols="6">
-              <app-checkbox id="generates_revenue" v-model="form.generates_revenue" title="Gera Receita?" :error-messages="form.errors.generates_revenue" />
+            <v-col cols="3">
+              <v-checkbox id="generates_revenue" v-model="form.generates_revenue" class="mt-6" label="Gera Receita?" :error-messages="form.errors.generates_revenue" />
             </v-col>
           </v-row>
         </v-card-text>
         <v-card-actions class="mx-4">
           <v-spacer />
-          <Link :href="route('orders.soStatus.index')" as="div">
+          <!-- <Link :href="route('orders.soStatus.index')" as="div">
             <v-btn class="mr-2" variant="text">Cancelar</v-btn>
-          </Link>
-          <v-btn type="submit" color="primary" variant="flat" :loading="form.processing">Salvar</v-btn>
+          </Link> -->
+          <v-btn type="submit" color="primary" variant="text" :loading="form.processing">Salvar</v-btn>
         </v-card-actions>
       </v-card>
     </v-form>
