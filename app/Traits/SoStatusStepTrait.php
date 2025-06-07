f19a52f821f729874106ae5f3a4aef52cdfdd38a
kallbuloso <kallbuloso@gmail.com>
2025-06-06 23:23:27 -0300

    feat: adicionar trait SoStatusStepTrait com uso de HasFactory e traits adicionais

---
+++ app/Traits/SoStatusStepTrait.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+trait SoStatusStepTrait
+{
+    use HasFactory, TenantebleTrait, DatesModelTraits;
+    //
+}
