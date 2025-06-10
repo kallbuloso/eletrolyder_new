74db5924f5a12d8882484e5a4363925a251e99c6
kallbuloso <kallbuloso@gmail.com>
2025-06-09 21:50:06 -0300

    feat: atualizar regras de validação para campos obrigatórios no SoStatusStepRequest

--- app/Http/Requests/SoStatusStepRequest.php
+++ app/Http/Requests/SoStatusStepRequest.php
@@ -7,24 +7,24 @@ use Illuminate\Foundation\Http\FormRequest;
 class SoStatusStepRequest extends FormRequest
 {
   /**
-    * Determine if the user is authorized to make this request.
-    */
+   * Determine if the user is authorized to make this request.
+   */
   public function authorize(): bool
   {
     return true;
   }
 
   /**
-    * Get the validation rules that apply to the request.
-    *
-    * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-    */
+   * Get the validation rules that apply to the request.
+   *
+   * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+   */
   public function rules(): array
   {
     return [
-			'tenant_id' => ['nullable', 'integer'],
-			'so_status_id' => ['nullable', 'integer'],
-			'description' => ['required', 'string'],
+      'tenant_id' => ['required', 'integer'],
+      'so_status_id' => ['required', 'integer'],
+      'description' => ['required', 'string'],
     ];
   }
 }
