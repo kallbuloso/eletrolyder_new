f81ae55b8eb1197334ba43861d3c8f8b37c17312
kallbuloso <kallbuloso@gmail.com>
2025-06-09 22:42:00 -0300

    feat: remover rotas desnecessárias do controlador SoStatusStep para simplificar a estrutura

--- routes/web.php
+++ routes/web.php
@@ -152,18 +152,15 @@ Route::middleware(['auth', 'verified'])->group(function () {
             Route::get('/', 'index')->name('index');
             Route::get('/create', 'create')->name('create');
             Route::post('/', 'store')->name('store');
-            Route::get('/show/{id}', 'show')->name('show');
             Route::get('/{id}/edit', 'edit')->name('edit');
             Route::put('/{id}', 'update')->name('update');
             Route::delete('/{id}', 'destroy')->name('destroy');
-            Route::get('/{id}/steps', [SoStatusStepController::class, 'index'])->name('steps');
         });
         // Route groups for SoStatusStep
         Route::controller(SoStatusStepController::class)->prefix('status-steps')->as('statusStep.')->group(function () {
             Route::get('/', 'index')->name('index');
             Route::get('/create/{data}', 'create')->name('create');
             Route::post('/', 'store')->name('store');
-            Route::get('/show/{id}', 'show')->name('show');
             Route::get('/{id}/edit', 'edit')->name('edit');
             Route::put('/{id}', 'update')->name('update');
             Route::delete('/{redirect}/{id}', 'destroy')->name('destroy');
