e300953a8c8338ec0852efd6359c2620c1b38b12
kallbuloso <kallbuloso@gmail.com>
2025-06-10 23:10:04 -0300

    feat: adicionar trait SoDevicesTypeTrait com injeção de dependência

---
+++ app/Traits/SoDevicesTypeTrait.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+trait SoDevicesTypeTrait
+{
+    use HasFactory, TenantebleTrait, DatesModelTraits;
+
+    // /**
+    //  * @return \Illuminate\Database\Eloquent\Relations\HasMany
+    //  */
+    // public function soDevices()
+    // {
+    //   return $this->hasMany(\App\Models\SoDevice::class, 'id', 'so_device_type_id');
+    // }
+}
