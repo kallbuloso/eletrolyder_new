11f7c34f146c6d915bf362278c79ea6e8af9b56f
kallbuloso <kallbuloso@gmail.com>
2025-06-11 20:03:27 -0300

    refactor: ajustar regras de validação no SoDevicesTypeRequest

--- app/Http/Requests/SoDevicesTypeRequest.php
+++ app/Http/Requests/SoDevicesTypeRequest.php
@@ -7,23 +7,24 @@ use Illuminate\Foundation\Http\FormRequest;
 class SoDevicesTypeRequest extends FormRequest
 {
   /**
-    * Determine if the user is authorized to make this request.
-    */
+   * Determine if the user is authorized to make this request.
+   */
   public function authorize(): bool
   {
     return true;
   }
 
   /**
-    * Get the validation rules that apply to the request.
-    *
-    * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-    */
+   * Get the validation rules that apply to the request.
+   *
+   * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+   */
   public function rules(): array
   {
     return [
-			'tenant_id' => ['nullable', 'integer'],
-			'description' => ['required', 'string'],
+      'tenant_id' => ['integer'],
+      'description' => ['required', 'string'],
+      'is_active' => ['boolean'],
     ];
   }
 }
