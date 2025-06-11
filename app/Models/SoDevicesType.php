b25f7c404af03762b49322a6f2c0c9133d1f3235
kallbuloso <kallbuloso@gmail.com>
2025-06-10 23:09:42 -0300

    feat: adicionar modelo SoDevicesType com atributos e configurações iniciais

---
+++ app/Models/SoDevicesType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\SoDevicesTypeTrait;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class SoDevicesType
+ *
+ * @property $id
+ * @property $tenant_id
+ * @property $description
+ * @property $created_at
+ * @property $updated_at
+ *
+ * @property Tenant $tenant
+ * @property Main.soDevice[] $main.soDevices
+ * @package App
+ * @mixin \Illuminate\Database\Eloquent\Builder
+ */
+class SoDevicesType extends Model
+{
+  use SoDevicesTypeTrait;
+
+  protected $table = "so_devices_type";
+
+  protected $perPage = 10;
+
+  protected $guarded = ['id'];
+
+  /**
+   * Attributes that should be mass-assignable.
+   *
+   * @var array
+   */
+  protected $fillable = [
+    'tenant_id',
+    'description'
+  ];
+
+  protected $searchable = [
+    'tenant_id',
+    'description'
+  ];
+}
