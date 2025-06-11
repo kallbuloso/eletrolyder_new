0d73e288ddc50e4eaa6dd2c0a55649c35cd4abb0
kallbuloso <kallbuloso@gmail.com>
2025-06-10 23:12:34 -0300

    feat: criar componente Create.vue para gerenciamento de status de ordens de serviço

---
+++ resources/js/Pages/ServicesOrder/Settings/StatusStep/Create.vue
@@ -0,0 +1,53 @@
+<script setup>
+const props = defineProps({
+  title: {
+    type: String,
+    required: true
+  },
+  soStatusId: {
+    type: [Number, String],
+    required: true
+  },
+  errors: {
+    type: Object,
+    default: () => ({})
+  }
+})
+const form = useForm({
+  so_status_id: props.soStatusId,
+  tenant_id: usePage().props.auth.user.tenant_id,
+  description: ''
+})
+
+const submit = () => {
+  form.post(route('orders.statusStep.store'), {
+    onSuccess() {
+      form.reset()
+    }
+  })
+}
+
+onMounted(() => {
+  // console.log('SoStatusStep page Create mounted')
+})
+</script>
+
+<template>
+  <app-modal width="450">
+    <v-form @submit.prevent="submit">
+      <v-card prepend-icon="mdi-plus" :title="props.title">
+        <v-card-text class="mx-1">
+          <v-row>
+            <v-col cols="12">
+              <app-text-field id="description" v-model="form.description" label="Descrição" placeholder="Descrição do passo" :error-messages="form.errors.description" />
+            </v-col>
+          </v-row>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn type="submit" color="primary" :loading="form.processing">Salvar</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-form>
+  </app-modal>
+</template>
