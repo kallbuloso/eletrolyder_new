cf024779891a90e71a8c0704eaa6c62dd6d0149d
kallbuloso <kallbuloso@gmail.com>
2025-06-17 19:46:40 -0300

    feat: criar trait SoDeviceTrait com relacionamentos para SoDevicesType e ServiceOrder

---
+++ app/Traits/SoDeviceTrait.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+trait SoDeviceTrait
+{
+    use HasFactory, TenantebleTrait, DatesModelTraits;
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function soDevicesType()
+    {
+        return $this->belongsTo(\App\Models\SoDevicesType::class, 'so_device_type_id', 'id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function serviceOrders()
+    {
+        // return $this->hasMany(\App\Models\ServiceOrder::class, 'id', 'so_device_id');
+    }
+}
