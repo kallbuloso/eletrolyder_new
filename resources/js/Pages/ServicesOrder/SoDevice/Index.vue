38da7c56bce7ee0a4cc4a959f8e13574a3412838
kallbuloso <kallbuloso@gmail.com>
2025-06-17 20:50:31 -0300

    feat: atualizar ícone e texto do botão de criação na página SoDevice

--- resources/js/Pages/ServicesOrder/SoDevice/Index.vue
+++ resources/js/Pages/ServicesOrder/SoDevice/Index.vue
@@ -7,15 +7,11 @@ const props = defineProps({
   routeDefault: {
     type: String,
     required: true
-  },
-  count: {
-    type: Number,
-    default: 0
   }
 })
 
 const pageTitle = props.title
-const pageIcon = 'iconify:mingcute:device-line'
+const pageIcon = 'iconify:tabler:device-tv-old'
 
 function routeBase(name) {
   return props.routeDefault + name
@@ -23,23 +19,11 @@ function routeBase(name) {
 
 // Definição das colunas
 const headers = ref([
-  // Todo: ajustar os headers
-  { title: 'ID', key: 'id' },
-	{ title: 'Tenant Id', key: 'tenant_id' },
-	{ title: 'So Device Type Id', key: 'so_device_type_id' },
-	{ title: 'Description', key: 'description' },
-	{ title: 'Brand', key: 'brand' },
-	{ title: 'Model', key: 'model' },
-	{ title: 'Serial Number', key: 'serial_number' },
-	{ title: 'Damages', key: 'damages' },
-	{ title: 'Accessories', key: 'accessories' },
-	{ title: 'Notes', key: 'notes' },
-	{ title: 'Warranty Provider', key: 'warranty_provider' },
-	{ title: 'Purchase Date', key: 'purchase_date' },
-	{ title: 'Reseller', key: 'reseller' },
-	{ title: 'Invoice Number', key: 'invoice_number' },
-	{ title: 'Warranty Certificate', key: 'warranty_certificate' },
-  { title: 'Action', key: 'action', sortable: false, align: 'end' },
+  { title: 'Dispositivo', key: 'so_device_type_id' },
+  { title: 'Descrição', key: 'description' },
+  { title: 'Marca', key: 'brand' },
+  { title: 'Modelo', key: 'model' },
+  { title: 'Ação', key: 'action', sortable: false, align: 'end' }
 ])
 
 // Estado da tabela
@@ -125,86 +109,68 @@ function editItem(item) {
 
 function createItem() {
   if (can('soDevice', 'criar')) {
-    router.get(route(routeBase('create')'))
+    router.get(route(routeBase('create')))
   } else {
     swToast(`Você não tem permissão para criar ${pageTitle}.`, 'error', 3000)
   }
 }
-
-onMounted(() => {
-  // console.log(route(routeBase('index')))
-  // console.log('SoDevice page mounted')
-})
 </script>
 
 <template layout="AppShell,AuthenticatedLayout">
-  <template v-if="props.count > 0">
-    <v-card class="mx-auto" width="800" prepend-icon="mdi-account-lock" :title="pageTitle">
-      <template #append>
-        <v-btn prepend-icon="mdi-plus" color="primary" variant="text" @click="createItem()">Novo</v-btn>
+  <v-card class="mx-auto" :prepend-icon="pageIcon" :title="pageTitle">
+    <template #append>
+      <v-btn prepend-icon="mdi-plus" color="primary" variant="text" @click="createItem()">Adicionar {{ pageTitle }}</v-btn>
+    </template>
+    <v-card-text class="d-flex">
+      <v-row>
+        <v-col cols="12" lg="8" md="8" sm="6">
+          <v-text-field v-model="search" label="Procurar" prepend-inner-icon="mdi-magnify" hide-details clearable />
+        </v-col>
+        <v-col v-if="content.total > 10" cols="12" lg="4" md="4" sm="6">
+          <v-select v-model="itemsPerPage" :items="[10, 15, 25, 35, 50, 100]" label="Itens por Página" width="150px"></v-select>
+        </v-col>
+      </v-row>
+    </v-card-text>
+    <!--
+      <pre>{{ props.data }}</pre>
+      -->
+    <v-card-item>
+      <v-data-table-server
+        :page="page"
+        :items="content.data"
+        :items-length="Number(content.total)"
+        :headers="headers"
+        :search="search"
+        :show-select="false"
+        :items-per-page="itemsPerPage"
+        loading-text="Carregando, por favor aguarde..."
+        :loading="isLoadingTable"
+        @update:options="loadItems"
+      >
+        <template #loading>
+          <v-skeleton-loader type="table-row@5" />
+        </template>
+        <!--<template #item.gender="{ item }">
+          {{ item.gender == 'male' ? 'Masculino' : 'Feminino' }}
+        </template>-->
+        <template #item.action="{ item }">
+          <!--
+              <v-icon class="ml-2" color="primary" icon="mdi-eye" size="small" @click="showItem(item)" />
+            -->
+          <v-icon v-if="can('soDevice', 'editar')" color="warning" icon="mdi-pencil" size="small" @click="editItem(item)" />
+          <v-icon v-if="can('soDevice', 'excluir')" class="ml-1" color="error" icon="mdi-delete" size="small" @click="deleteItem(item)" />
+        </template>
+        <template #bottom>
+          <v-divider />
+        </template>
+      </v-data-table-server>
+    </v-card-item>
+    <v-card-actions>
+      <template v-if="content.total > 10 && itemsPerPage < content.total">
+        <v-list-item :title="`Página ${content.current_page} de ${content.last_page}`" :subtitle="`Total de ${formatCount(content.total)} ${pageTitle}`" />
+        <v-spacer />
+        <v-pagination v-model="page" :length="content.last_page" :total-visible="4" size="small" rounded></v-pagination>
       </template>
-      <v-card-text class="d-flex">
-        <v-row>
-          <v-col cols="12" lg="8" md="8" sm="6">
-            <v-text-field v-model="search" label="Procurar" prepend-inner-icon="mdi-magnify" hide-details clearable />
-          </v-col>
-          <v-col v-if="content.total > 10" cols="12" lg="4" md="4" sm="6">
-            <v-select v-model="itemsPerPage" :items="[10, 15, 25, 35, 50, 100]" label="Itens por Página" width="150px"></v-select>
-          </v-col>
-        </v-row>
-      </v-card-text>
-      <!--
-        <pre>{{ props.data }}</pre>
-        -->
-      <v-card-item>
-        <v-data-table-server
-          :page="page"
-          :items="content.data"
-          :items-length="Number(content.total)"
-          :headers="headers"
-          :search="search"
-          :show-select="false"
-          :items-per-page="itemsPerPage"
-          loading-text="Carregando, por favor aguarde..."
-          :loading="isLoadingTable"
-          @update:options="loadItems"
-        >
-          <template #loading>
-            <v-skeleton-loader type="table-row@5" />
-          </template>
-          <!--<template #item.gender="{ item }">
-            {{ item.gender == 'male' ? 'Masculino' : 'Feminino' }}
-          </template>-->
-          <template #item.action="{ item }">
-            <!--
-                <v-icon class="ml-2" color="primary" icon="mdi-eye" size="small" @click="showItem(item)" />
-              -->
-            <v-icon v-if="can('soDevice', 'editar')" color="warning" icon="mdi-pencil" size="small" @click="editItem(item)" />
-            <v-icon v-if="can('soDevice', 'excluir')" class="ml-1" color="error" icon="mdi-delete" size="small" @click="deleteItem(item)" />
-          </template>
-          <template #bottom>
-            <v-divider />
-          </template>
-        </v-data-table-server>
-      </v-card-item>
-      <v-card-actions>
-        <template v-if="content.total > 10 && itemsPerPage < content.total">
-          <v-list-item :title="`Página ${content.current_page} de ${content.last_page}`" :subtitle="`Total de ${formatCount(content.total)} ${pageTitle}`" />
-          <v-spacer />
-          <v-pagination v-model="page" :length="content.last_page" :total-visible="4" size="small" rounded></v-pagination>
-        </template>
-      </v-card-actions>
-    </v-card>
-  </template>
-  <template v-else>
-    <v-row align="center" justify="center" style="height: 70vh">
-      <v-empty-state :headline="pageTitle" title="Nenhum registro encontrado.">
-        <template #media>
-          <v-icon :icon="pageIcon" />
-        </template>
-        // Todo: alterar (Adicionar) abaixo
-        <v-btn prepend-icon="mdi-plus" color="primary" variant="flat" @click="createItem()">Adicionar Status</v-btn>
-      </v-empty-state>
-    </v-row>
-  </template>
+    </v-card-actions>
+  </v-card>
 </template>
