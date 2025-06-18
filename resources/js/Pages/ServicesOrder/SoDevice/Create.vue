de5f02d668617331d7a614d94f1f37777e376aa4
kallbuloso <kallbuloso@gmail.com>
2025-06-18 20:35:52 -0300

    feat: adicionar campos e componentes ao formulário de criação de dispositivos

--- resources/js/Pages/ServicesOrder/SoDevice/Create.vue
+++ resources/js/Pages/ServicesOrder/SoDevice/Create.vue
@@ -7,6 +7,10 @@ const props = defineProps({
   routeDefault: {
     type: String,
     required: true
+  },
+  soDevicesType: {
+    type: Array,
+    required: true
   }
 })
 
@@ -18,7 +22,6 @@ function routeBase(name) {
 
 // Todo: Ajustar o form
 const form = useForm({
-  tenant_id: '',
   so_device_type_id: '',
   description: '',
   brand: '',
@@ -44,70 +47,91 @@ const submit = () => {
 }
 
 onMounted(() => {
-  // console.log('SoDevice page Create mounted')
+  // console.log(props.soDevicesType)
 })
 </script>
 
 <template>
-  <app-modal width="450">
+  <app-modal width="800">
     <v-form @submit.prevent="submit">
       <v-card prepend-icon="mdi-plus" :title="pageTitle">
         <v-card-text class="mx-1">
-          <!-- Todo: Ajustar fields do form -->
           <v-row>
-            <v-col cols="12">
-              <v-text-field id="tenant_id" v-model="form.tenant_id" label="Tenant Id" placeholder="Tenant Id" :error-messages="form.errors.tenant_id" />
-            </v-col>
-            <v-col cols="12">
-              <v-text-field
+            <v-col cols="4">
+              <app-autocomplete
                 id="so_device_type_id"
                 v-model="form.so_device_type_id"
-                label="So Device Type Id"
-                placeholder="So Device Type Id"
+                label="Tipo de Dispositivo"
+                :items="props.soDevicesType"
+                item-value="id"
+                item-title="description"
+                placeholder="TV, Computador, Impressora, etc."
                 :error-messages="form.errors.so_device_type_id"
               />
             </v-col>
-            <v-col cols="12">
-              <v-text-field id="description" v-model="form.description" label="Description" placeholder="Description" :error-messages="form.errors.description" />
+            <v-col cols="8">
+              <app-name-field id="description" v-model="form.description" label="Descrição" placeholder="LED 32, Notebook, etc." :error-messages="form.errors.description" />
+            </v-col>
+            <!-- <pre>{{ form.so_device_type_id }}</pre> -->
+            <v-col cols="5">
+              <!-- Todo: adicionar botão com etiqueta rasurada ou sem marca -->
+              <app-name-field id="brand" v-model="form.brand" label="Marca" placeholder="ex: Samsung" :error-messages="form.errors.brand" />
+            </v-col>
+            <v-col cols="7">
+              <!-- Todo: adicionar botão com etiqueta rasurada ou sem modelo -->
+              <app-text-field id="model" v-model="form.model" label="Modelo" placeholder="ex: Galaxy S21" :error-messages="form.errors.model" />
+            </v-col>
+            <v-col cols="5">
+              <!-- Todo: adicionar botão com etiqueta rasurada ou sem número de série -->
+              <app-upper-field
+                id="serial_number"
+                v-model="form.serial_number"
+                label="Número de Série / IMEI"
+                placeholder="Número de Série"
+                :error-messages="form.errors.serial_number"
+              />
+            </v-col>
+            <v-col cols="7">
+              <app-text-field
+                id="damages"
+                v-model="form.damages"
+                label="Danos visíveis"
+                placeholder="ex: Tela trincada, Gabinete riscado, faltando peças"
+                :error-messages="form.errors.damages"
+              />
+            </v-col>
+            <v-col cols="6">
+              <app-text-field
+                id="accessories"
+                v-model="form.accessories"
+                label="Acessórios"
+                placeholder="ex: suporte, cabo de força, base, etc."
+                :error-messages="form.errors.accessories"
+              />
+            </v-col>
+            <v-col cols="6">
+              <app-text-field id="notes" v-model="form.notes" label="Observações" placeholder="Notas sobre o dispositivo" :error-messages="form.errors.notes" />
             </v-col>
             <v-col cols="12">
-              <v-text-field id="brand" v-model="form.brand" label="Brand" placeholder="Brand" :error-messages="form.errors.brand" />
-            </v-col>
-            <v-col cols="12">
-              <v-text-field id="model" v-model="form.model" label="Model" placeholder="Model" :error-messages="form.errors.model" />
-            </v-col>
-            <v-col cols="12">
-              <v-text-field id="serial_number" v-model="form.serial_number" label="Serial Number" placeholder="Serial Number" :error-messages="form.errors.serial_number" />
-            </v-col>
-            <v-col cols="12">
-              <v-text-field id="damages" v-model="form.damages" label="Damages" placeholder="Damages" :error-messages="form.errors.damages" />
-            </v-col>
-            <v-col cols="12">
-              <v-text-field id="accessories" v-model="form.accessories" label="Accessories" placeholder="Accessories" :error-messages="form.errors.accessories" />
-            </v-col>
-            <v-col cols="12">
-              <v-text-field id="notes" v-model="form.notes" label="Notes" placeholder="Notes" :error-messages="form.errors.notes" />
-            </v-col>
-            <v-col cols="12">
-              <v-text-field
+              <app-text-field
                 id="warranty_provider"
                 v-model="form.warranty_provider"
-                label="Warranty Provider"
-                placeholder="Warranty Provider"
+                label="Fornecedor da Garantia"
+                placeholder="Fornecedor da Garantia"
                 :error-messages="form.errors.warranty_provider"
               />
             </v-col>
             <v-col cols="12">
-              <v-text-field id="purchase_date" v-model="form.purchase_date" label="Purchase Date" placeholder="Purchase Date" :error-messages="form.errors.purchase_date" />
+              <app-text-field id="purchase_date" v-model="form.purchase_date" label="Purchase Date" placeholder="Purchase Date" :error-messages="form.errors.purchase_date" />
             </v-col>
             <v-col cols="12">
-              <v-text-field id="reseller" v-model="form.reseller" label="Reseller" placeholder="Reseller" :error-messages="form.errors.reseller" />
+              <app-text-field id="reseller" v-model="form.reseller" label="Reseller" placeholder="Reseller" :error-messages="form.errors.reseller" />
             </v-col>
             <v-col cols="12">
-              <v-text-field id="invoice_number" v-model="form.invoice_number" label="Invoice Number" placeholder="Invoice Number" :error-messages="form.errors.invoice_number" />
+              <app-text-field id="invoice_number" v-model="form.invoice_number" label="Invoice Number" placeholder="Invoice Number" :error-messages="form.errors.invoice_number" />
             </v-col>
             <v-col cols="12">
-              <v-text-field
+              <app-text-field
                 id="warranty_certificate"
                 v-model="form.warranty_certificate"
                 label="Warranty Certificate"
