8fd3b761f7ac0f3ca8fe778ae511bfb286cd4b3b
kallbuloso <kallbuloso@gmail.com>
2025-08-22 20:50:47 -0300

    feat: adicionar lógica para exibir tipo de dispositivo e melhorar a estrutura do componente

--- resources/js/Pages/ServicesOrder/SoDevice/Index.vue
+++ resources/js/Pages/ServicesOrder/SoDevice/Index.vue
@@ -7,6 +7,14 @@ const props = defineProps({
   routeDefault: {
     type: String,
     required: true
+  },
+  soDevicesType: {
+    type: Array,
+    required: true
+  },
+  count: {
+    type: Number,
+    default: 0
   }
 })
 
@@ -98,6 +106,10 @@ function editItem(item) {
   }
 }
 
+function getDeviceType(id) {
+  const type = props.soDevicesType.find((item) => item.id === id)
+  return type ? type.description : '-'
+}
 // Todo: Remover, se não usado
 // function showItem(item) {
 //   if (can('soDevice', 'ver')) {
@@ -117,61 +129,67 @@ function createItem() {
 </script>
 
 <template layout="AppShell,AuthenticatedLayout">
-  <v-card class="mx-auto" :prepend-icon="pageIcon" :title="pageTitle">
-    <template #append>
-      <v-btn prepend-icon="mdi-plus" color="primary" variant="text" @click="createItem()">Adicionar {{ pageTitle }}</v-btn>
-    </template>
-    <v-card-text class="d-flex">
-      <v-row>
-        <v-col cols="12" lg="8" md="8" sm="6">
-          <v-text-field v-model="search" label="Procurar" prepend-inner-icon="mdi-magnify" hide-details clearable />
-        </v-col>
-        <v-col v-if="content.total > 10" cols="12" lg="4" md="4" sm="6">
-          <v-select v-model="itemsPerPage" :items="[10, 15, 25, 35, 50, 100]" label="Itens por Página" width="150px"></v-select>
-        </v-col>
-      </v-row>
-    </v-card-text>
-    <!--
-      <pre>{{ props.data }}</pre>
-      -->
-    <v-card-item>
-      <v-data-table-server
-        :page="page"
-        :items="content.data"
-        :items-length="Number(content.total)"
-        :headers="headers"
-        :search="search"
-        :show-select="false"
-        :items-per-page="itemsPerPage"
-        loading-text="Carregando, por favor aguarde..."
-        :loading="isLoadingTable"
-        @update:options="loadItems"
-      >
-        <template #loading>
-          <v-skeleton-loader type="table-row@5" />
-        </template>
-        <template #item.so_device_type_id="{ item }">
-          {{ item.soDevicesType?.description || '-' }}
-        </template>
-        <template #item.action="{ item }">
-          <!--
-              <v-icon class="ml-2" color="primary" icon="mdi-eye" size="small" @click="showItem(item)" />
-            -->
-          <v-icon v-if="can('soDevice', 'editar')" color="warning" icon="mdi-pencil" size="small" @click="editItem(item)" />
-          <v-icon v-if="can('soDevice', 'excluir')" class="ml-1" color="error" icon="mdi-delete" size="small" @click="deleteItem(item)" />
-        </template>
-        <template #bottom>
-          <v-divider />
-        </template>
-      </v-data-table-server>
-      <pre>{{ content.data }}</pre>
-    </v-card-item>
-    <v-card-actions>
-      <template v-if="content.total > 10 && itemsPerPage < content.total">
-        <v-list-item :title="`Página ${content.current_page} de ${content.last_page}`" :subtitle="`Total de ${formatCount(content.total)} ${pageTitle}`" />
-        <v-spacer />
-        <v-pagination v-model="page" :length="content.last_page" :total-visible="4" size="small" rounded></v-pagination>
+  <template v-if="Number(props.count) > 0">
+    <v-card class="mx-auto" :prepend-icon="pageIcon" :title="pageTitle">
+      <template #append>
+        <v-btn prepend-icon="mdi-plus" color="primary" variant="text" @click="createItem()">Adicionar {{ pageTitle }}</v-btn>
       </template>
-    </v-card-actions>
-  </v-card>
+      <v-card-text v-if="content.total > 10" class="d-flex">
+        <v-row>
+          <v-col cols="12" lg="8" md="8" sm="6">
+            <v-text-field v-model="search" label="Procurar" prepend-inner-icon="mdi-magnify" hide-details clearable />
+          </v-col>
+          <v-col v-if="content.total > 10" cols="12" lg="4" md="4" sm="6">
+            <v-select v-model="itemsPerPage" :items="[10, 15, 25, 35, 50, 100]" label="Itens por Página" width="150px"></v-select>
+          </v-col>
+        </v-row>
+      </v-card-text>
+      <v-card-item>
+        <v-data-table-server
+          :page="page"
+          :items="content.data"
+          :items-length="Number(content.total)"
+          :headers="headers"
+          :search="search"
+          :show-select="false"
+          :items-per-page="itemsPerPage"
+          loading-text="Carregando, por favor aguarde..."
+          :loading="isLoadingTable"
+          @update:options="loadItems"
+        >
+          <template #loading>
+            <v-skeleton-loader type="table-row@5" />
+          </template>
+          <template #item.so_device_type_id="{ item }">
+            {{ getDeviceType(item.so_device_type_id) }}
+          </template>
+          <template #item.action="{ item }">
+            <v-icon v-if="can('soDevice', 'editar')" color="warning" icon="mdi-pencil" size="small" @click="editItem(item)" />
+            <v-icon v-if="can('soDevice', 'excluir')" class="ml-1" color="error" icon="mdi-delete" size="small" @click="deleteItem(item)" />
+          </template>
+          <template #bottom>
+            <v-divider />
+          </template>
+        </v-data-table-server>
+      </v-card-item>
+      <v-card-actions>
+        <template v-if="content.total > 10 && itemsPerPage < content.total">
+          <v-list-item :title="`Página ${content.current_page} de ${content.last_page}`" :subtitle="`Total de ${formatCount(content.total)} ${pageTitle}`" />
+          <v-spacer />
+          <v-pagination v-model="page" :length="content.last_page" :total-visible="4" size="small" rounded></v-pagination>
+        </template>
+      </v-card-actions>
+    </v-card>
+  </template>
+  <template v-else>
+    <v-row align="center" justify="center" style="height: 70vh">
+      <v-empty-state :headline="pageTitle" title="Nenhum registro encontrado.">
+        <template #media>
+          <v-icon :icon="pageIcon" />
+        </template>
+        <!-- Todo: alterar (Adicionar) abaixo -->
+        <v-btn prepend-icon="mdi-plus" color="primary" variant="flat" @click="createItem()">Adicionar {{ pageTitle }}</v-btn>
+      </v-empty-state>
+    </v-row>
+  </template>
 </template>
